5d35735e7de1434037309e2524db05c1abffec89
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-12-09 11:02:58 +0000

    Add 2025 Constants File

---
+++ constants2025.py
@@ -0,0 +1,56 @@
+PLAYERS_TO_COLOURS = {
+    "Benedict": (4, 2, 115), #"midnightblue",
+    "Carla": (232, 0, 45), #"red",
+    "Damian": (250, 194, 5), #"yellow",
+    "David": (19, 234, 201), #"cyan",
+    "Jarek": (2, 143, 30), #"forestgreen",
+    "Josh": (255, 91, 0), #"orange",
+    "Suley": (154, 14, 234) #"purple"
+}
+
+TEAMS_TO_COLOURS = {
+    "Red Bull" : "#3671C6",
+    "Mercedes": "#27F4D2",
+    "Ferrari": "#E8002D",
+    "McLaren": "#FF8000",
+    "Aston Martin": "#229971",
+    "Alpine": "#2173B8",
+    "Williams": "#64C4FF",
+    "Visa RB": "#6692FF",
+    "Stake": "#52E252",
+    "Haas": "#B6BABD"
+}
+
+RACES = [
+    "Bahrain",
+    "Saudi Arabia",
+    "Australia",
+    "Japan",
+    "China",
+    "Miami",
+    "Imola",
+    "Monaco",
+    "Canada",
+    "Spain",
+    "Austria",
+    "Silverstone",
+    "Hungary",
+    "Spa",
+    "Netherlands",
+    "Monza",
+    "Baku",
+    "Singapore",
+    "COTA",
+    "Mexico",
+    "Brazil",
+    "Las Vegas",
+    "Qatar",
+    "Abu Dhabi"
+]
+
+NUMBER_OF_DRIVERS = 20
+NUMBER_OF_PLAYERS = len(PLAYERS_TO_COLOURS)
+NUMBER_OF_TEAMS = len(TEAMS_TO_COLOURS)
+NUMBER_OF_RACES = len(RACES)
+
+ROAD_COLOUR = (0.35, 0.35, 0.35)
